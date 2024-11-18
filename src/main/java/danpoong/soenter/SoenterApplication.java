72f2db3d8e93aeb40d3111d6665e8a00a3380f3a
haeun1107 <haeun9394@gmail.com>
2024-11-18 09:46:15 +0900

    Init: 프로젝트 초기 세팅

---
+++ src/main/java/danpoong/soenter/SoenterApplication.java
@@ -0,0 +1,13 @@
+package danpoong.soenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/danpoong/soenter/SoenterApplicationTests.java
@@ -0,0 +1,13 @@
+package danpoong.soenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
